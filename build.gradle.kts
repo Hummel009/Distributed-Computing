501a0d430849871c05b7eb18f61bfe754503a54f
Hummel009 <hummel.turbamentum@gmail.com>
2024-05-23 12:42:29 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.9.24" apply false
-	id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
+	id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
+	id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
 	id("io.ktor.plugin") version "2.3.10" apply false
 }
